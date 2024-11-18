ce3f967117c6d57f80de009b41099f6eab96895c
Mateus <mateus.ssilva2002@gmail.com>
2024-11-18 20:49:14 -0300

    project started

---
+++ src/test/java/com/mateus/aula_jpaRepository/AulaJpaRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.aula_jpaRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaJpaRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
